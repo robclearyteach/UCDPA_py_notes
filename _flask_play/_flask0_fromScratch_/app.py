204b899cd2166bbba1e8d935dac360f31fd30c1a
robteach <robert.cleary.teach@gmail.com>
2024-06-18 19:54:08 +0100

    update with url_for() and url_for('static'...) examples

--- _flask_play/_flask0_fromScratch_/app.py
+++ _flask_play/_flask0_fromScratch_/app.py
@@ -1,5 +1,5 @@
 ## install flask first
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,7 +7,11 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
 	return render_template("home.html")
-#project/templates/home.html
+	#project/templates/home.html
+
+@app.route("/contact")
+def tiddlywinks():
+	return "<h1> Contact page displaying...</h1>"
 
 if __name__ == '__main__':
 	app.run(debug=True, port=8080)
