8b0b2bd85a204bce8dc535aa6cff14eae653b3cd
robteach <robert.cleary.teach@gmail.com>
2024-06-25 19:59:12 +0100

    add updated GET/POST example

--- _flask_play/_flask4_GET_POST/app.py
+++ _flask_play/_flask4_GET_POST/app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/', methods=["GET"]) 
+def index_get():
+    if request.args:
+        name_submitted = request.args.get("name")
+        return render_template('index.html', name=name_submitted, render="GET")
+    else:
         return render_template('index.html')
-
+    
+@app.route('/', methods=["POST"]) 
+def index_post():
+    name_posted = request.form.get("name")
+    return render_template("index.html", name=name_posted, render="POST")
+    
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
